d374b0b2f8c71cb127a11da9b5639602ab520bdd
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-20 07:57:57 -0500

    clase cuadrilatero

--- src/cuadrilatero.java
+++ src/cuadrilatero.java
@@ -1,2 +1,27 @@
-public class cuadrilatero {
+public class cuadrilatero extends FIGURAS{
+    private String Nombre;
+    private int T_lado;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getT_lado() {
+        return T_lado;
+    }
+
+    public void setT_lado(int t_lado) {
+        T_lado = t_lado;
+    }
+
+    public cuadrilatero(String dimencion, int n_lados, String nombre, int t_lado) {
+        super(dimencion, n_lados);
+        Nombre = nombre;
+        T_lado = t_lado;
+    }
 }
+
